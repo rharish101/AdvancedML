b0f0fd4826beeac011201a57dd8fa3b1b34bb0f2
Harish Rajagopal <harish.rajagopals@gmail.com>
2020-10-09 15:41:47 +0200

    Switched to sklearn's imputer
    This is more flexible, and allows for making it more complex later on.

--- src/task1.py
+++ src/task1.py
@@ -1,6 +1,6 @@
 """The entry point for the scripts for Task 1."""
 import numpy
-from pandas import DataFrame
+from sklearn.impute import SimpleImputer
 
 from typings import CSVData, CSVHeader
 from utilities.data import create_submission_file, print_array, print_array_statistics, read_csv
@@ -9,33 +9,26 @@ TASK_DATA_DIRECTORY = "data/task1"
 TRAINING_DATA_PATH = f"{TASK_DATA_DIRECTORY}/X_train.csv"
 TRAINING_LABELS_PATH = f"{TASK_DATA_DIRECTORY}/y_train.csv"
 TEST_DATA_PATH = f"{TASK_DATA_DIRECTORY}/X_test.csv"
-
 OUTPUT_FILE = "dist/submission1.csv"
 
 
 def __main():
     # Read in data
-    (training_data, training_header) = read_csv(TRAINING_DATA_PATH)
-    (training_labels, _labels_header) = read_csv(TRAINING_LABELS_PATH)
-    (test_data, test_header) = read_csv(TEST_DATA_PATH)
+    X_train, _ = read_csv(TRAINING_DATA_PATH)
+    Y_train, _ = read_csv(TRAINING_LABELS_PATH)
+    X_test, _ = read_csv(TEST_DATA_PATH)
 
-    if training_data is None or training_labels is None or test_data is None:
-        print("There was a problem with reading CSV data. Exiting...")
-        exit(1)
+    if X_train is None or Y_train is None or X_test is None:
+        raise RuntimeError("There was a problem with reading CSV data")
 
-    # First we fill in missing values to the median
-    X_train = DataFrame(training_data, columns=training_header)
-    X_train.fillna(X_train.median())
-
-    # Y_train = DataFrame(training_labels, columns=labels_header)
-
-    X_test = DataFrame(test_data, columns=test_header)
-    X_test.fillna(X_train.median())
+    # We can substitute this for a more complex imputer later on
+    imputer = SimpleImputer(strategy="median")
+    X_train = imputer.fit_transform(X_train)
 
     create_submission_file(OUTPUT_FILE, numpy.array([]))
 
 
-def __print_data(data: CSVData, labels: CSVData, header: CSVHeader):
+def __print_data(data: CSVData, labels: CSVData, header: CSVHeader) -> None:
     # Print preview of data
     print_array(data, header)
     print_array(labels, header)
