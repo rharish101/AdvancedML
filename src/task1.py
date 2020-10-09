b6787dbf331e6d7a110a5abe0303b901bf49b8e8
dtrujillo <dtrujillo@student.ethz.ch>
2020-10-09 15:08:56 +0200

    impute using median

--- src/task1.py
+++ src/task1.py
@@ -1,5 +1,6 @@
 """The entry point for the scripts for Task 1."""
 import numpy
+from pandas import DataFrame
 
 from typings import CSVData, CSVHeader
 from utilities.data import create_submission_file, print_array, print_array_statistics, read_csv
@@ -15,17 +16,22 @@ OUTPUT_FILE = "dist/submission1.csv"
 def __main():
     # Read in data
     (training_data, training_header) = read_csv(TRAINING_DATA_PATH)
-    (training_labels, _) = read_csv(TRAINING_LABELS_PATH)
-    (test_data, _) = read_csv(TEST_DATA_PATH)
+    (training_labels, _labels_header) = read_csv(TRAINING_LABELS_PATH)
+    (test_data, test_header) = read_csv(TEST_DATA_PATH)
 
     if training_data is None or training_labels is None or test_data is None:
         print("There was a problem with reading CSV data. Exiting...")
         exit(1)
 
-    # Comment out line below to remove diagnostic data printing
-    __print_data(training_data, training_labels, training_header or ())
+    # First we fill in missing values to the median
+    X_train = DataFrame(training_data, columns=training_header)
+    X_train.fillna(X_train.median())
 
-    # Write results to a submission file
+    # Y_train = DataFrame(training_labels, columns=labels_header)
+
+    X_test = DataFrame(test_data, columns=test_header)
+    X_test.fillna(X_train.median())
+
     create_submission_file(OUTPUT_FILE, numpy.array([]))
 
 
