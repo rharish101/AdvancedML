73fccb574f0950a79aa072ddcc6fa3fcadc1357e
Harish Rajagopal <harish.rajagopals@gmail.com>
2020-11-28 18:51:54 +0100

    Added CUDA support for NN

--- src/task3_nn.py
+++ src/task3_nn.py
@@ -67,6 +67,8 @@ class NN(BaseClassifier):
         self.weight_decay = weight_decay
         self.balance_weights = balance_weights
 
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     def _init_model_optim(self, in_channels: int, num_classes: int) -> None:
         self.model = Sequential(
             Lambda(lambda x: x.permute(1, 2, 0)),  # [l, n, c] => [n, c, l]
@@ -77,7 +79,7 @@ class NN(BaseClassifier):
             GRU(128, 64),
             Lambda(lambda x: x[0][-1]),  # [l, n, c] => [n, c]
             Linear(64, num_classes),
-        )
+        ).to(self.device)
 
     def _gen_batches(
         self, X: List[CSVData], y: Optional[CSVData] = None
@@ -90,12 +92,12 @@ class NN(BaseClassifier):
             batch_indices = indices[i : i + self.batch_size]
             batch_X = pad_sequence(
                 [torch.from_numpy(X[i].astype(np.float32)) for i in batch_indices]
-            )
+            ).to(self.device)
 
             if y is None:
                 yield batch_X
             else:
-                batch_y = torch.from_numpy(y[batch_indices])
+                batch_y = torch.from_numpy(y[batch_indices]).to(self.device)
                 yield batch_X, batch_y
 
     def fit(self, X: List[CSVData], y: CSVData) -> None:
@@ -115,7 +117,7 @@ class NN(BaseClassifier):
         self._init_model_optim(in_channels, num_classes)
 
         if self.balance_weights:
-            class_weights = torch.from_numpy((1 / class_count).astype(np.float32))
+            class_weights = torch.from_numpy((1 / class_count).astype(np.float32)).to(self.device)
             # Normalize weights such that equal class frequencies imply 1:1:1 ratio
             class_weights /= class_weights.min()
         else:
@@ -136,7 +138,7 @@ class NN(BaseClassifier):
             ):
                 optim.zero_grad()
                 loss = loss_func(self.model(batch_X), batch_y)
-                running_loss += loss.detach().numpy()
+                running_loss += loss.detach()
                 loss.backward()
                 optim.step()
 
@@ -161,7 +163,7 @@ class NN(BaseClassifier):
         self.model.eval()
         with torch.no_grad():
             for batch_X in self._gen_batches(X):
-                pred.append(softmax_func(self.model(batch_X)).numpy())
+                pred.append(softmax_func(self.model(batch_X)).cpu().numpy())
 
         return np.concatenate(pred, axis=0)
 
